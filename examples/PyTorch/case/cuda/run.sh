e7bf1e1c0122437e0442ef6e063e976d7b47d22e
yunzhongOvO <misszhong98@163.com>
2023-05-15 14:11:35 +0000

    add schedule 3

--- examples/PyTorch/case/cuda/run.sh
+++ examples/PyTorch/case/cuda/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# FILE=colreduct_naive
-FILE=colreduct_transpose
+FILE=colreduct2_32x8
+# FILE=colreduct3
 
 nvcc -o ${FILE} ${FILE}.cu
 
-CUDA_VISIBLE_DEVICES=4 ./${FILE}
+CUDA_VISIBLE_DEVICES=5 ./${FILE}
 
 # CUDA_VISIBLE_DEVICES=4 nsys profile -f true --stats=true -o ${FILE} ./${FILE}
